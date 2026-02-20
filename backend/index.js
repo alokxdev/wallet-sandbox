140844834f2fd69a215c57696b5a80c17da63c51
Alok <alokforreal@gmail.com>
2026-02-20 19:23:24 +0530

    Add Express app initialization and API routing setup

--- backend/index.js
+++ backend/index.js
@@ -2,3 +2,6 @@ import express from "express";
 import mainRouter from "./routes/index.js";
 import dotenv from "dotenv";
 dotenv.config();
+
+const app = express();
+app.use("/api/v1", mainRouter);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,3 @@
 import { Router } from "express";
-
 export const router = Router();
-
 export default router;
